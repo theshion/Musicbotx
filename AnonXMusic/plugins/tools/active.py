ac39ebc28b33bd9e4b98113b26f179be546271c5
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2024-01-21 20:00:12 +0530

    Update active.py

--- AnonXMusic/plugins/tools/active.py
+++ AnonXMusic/plugins/tools/active.py
@@ -11,6 +11,18 @@ from AnonXMusic.utils.database import (
     remove_active_video_chat,
 )
 
+from typing import Union
+
+from pyrogram import filters, types
+from pyrogram.types import InlineKeyboardMarkup, Message
+
+from AnonXMusic.utils import help_pannel, ahelp_pannel
+from AnonXMusic.utils.database import get_lang
+from AnonXMusic.utils.decorators.language import LanguageStart, languageCB
+from AnonXMusic.utils.inline.help import help_back_markup, ahelp_back_markup, private_help_panel
+from AnonXMusic.utils.inline.start import exp_panel, feature_panel
+from config import BANNED_USERS, START_IMG_URL, SUPPORT_CHAT
+from strings import get_string, helpers, ai
 
 
 @app.on_callback_query(filters.regex("ahelp_callback") & ~BANNED_USERS)
