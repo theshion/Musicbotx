80fce33b5cf29a58083d940e9a218684d462942e
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2024-01-21 17:38:22 +0530

    Update help.py

--- AnonXMusic/plugins/bot/help.py
+++ AnonXMusic/plugins/bot/help.py
@@ -4,10 +4,10 @@ from pyrogram import filters, types
 from pyrogram.types import InlineKeyboardMarkup, Message
 
 from AnonXMusic import app
-from AnonXMusic.utils import help_pannel
+from AnonXMusic.utils import help_pannel, ahelp_pannel
 from AnonXMusic.utils.database import get_lang
 from AnonXMusic.utils.decorators.language import LanguageStart, languageCB
-from AnonXMusic.utils.inline.help import help_back_markup, private_help_panel
+from AnonXMusic.utils.inline.help import help_back_markup, ahelp_back_markup, private_help_panel
 from AnonXMusic.utils.inline.start import exp_panel, feature_panel
 from config import BANNED_USERS, START_IMG_URL, SUPPORT_CHAT
 from strings import get_string, helpers
@@ -160,12 +160,44 @@ async def noah_private(
             reply_markup=keyboard,
         )
 
+app.on_message(filters.command(["ahelp"]) & filters.private & ~BANNED_USERS)
+@app.on_callback_query(filters.regex("settings_back_helper") & ~BANNED_USERS)
+async def helper_private(
+    client: app, update: Union[types.Message, types.CallbackQuery]
+):
+    is_callback = isinstance(update, types.CallbackQuery)
+    if is_callback:
+        try:
+            await update.answer()
+        except:
+            pass
+        chat_id = update.message.chat.id
+        language = await get_lang(chat_id)
+        _ = get_string(language)
+        keyboard = ahelp_pannel(_, True)
+        await update.edit_message_text(
+            _["help_1"], reply_markup=keyboard
+        )
+    else:
+        try:
+            await update.delete()
+        except:
+            pass
+        language = await get_lang(update.chat.id)
+        _ = get_string(language)
+        keyboard = feature_pannel(_)
+        await update.reply_photo(
+            photo=START_IMG_URL,
+            caption=_["help_1"],
+            reply_markup=keyboard,
+        )
+
 @app.on_callback_query(filters.regex("ahelp_callback") & ~BANNED_USERS)
 @languageCB
 async def helper_cb(client, CallbackQuery, _):
     callback_data = CallbackQuery.data.strip()
     cb = callback_data.split(None, 1)[1]
-    keyboard = help_back_markup(_)
+    keyboard = ahelp_back_markup(_)
     if cb == "a1":
         await CallbackQuery.edit_message_text(ai.AF_1, reply_markup=keyboard)
     elif cb == "a2":
@@ -174,4 +206,5 @@ async def helper_cb(client, CallbackQuery, _):
         await CallbackQuery.edit_message_text(ai.AF_3, reply_markup=keyboard)
     elif cb == "a4":
         await CallbackQuery.edit_message_text(ai.AF_4, reply_markup=keyboard)
+
 
