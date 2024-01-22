f506b57a918898817997764234841097e6bd4089
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2024-01-22 12:33:30 +0530

    Update aiplay.py

--- AnonXMusic/plugins/play/aiplay.py
+++ AnonXMusic/plugins/play/aiplay.py
@@ -4,13 +4,11 @@ from AnonXMusic.core.call import Anony
 from AnonXMusic import Spotify, app
 from spotipy.oauth2 import SpotifyOAuth
 
-
 SPOTIFY_PLAYLISTS = {
     'English': 'https://open.spotify.com/playlist/0rAdSPycrFdaBXh9xSW3ap?si=hCVorPfNT8Ci_Uhzicx8qA',
     'Hindi': 'https://open.spotify.com/playlist/0XSjIw422sAwiKUMq4cm2l?si=pTIt8-UhQ7OebaiCt5wM7A',
     'OtherLanguages': 'https://open.spotify.com/artist/3kXvE7gEBfGkwDaknMngF7?si=VhIgr9TXRIa9_FJGgGr99A'
 }
-
 
 @app.on_message(filters.command("aiplay"))
 def aiplay(_, msg):
@@ -20,15 +18,9 @@ def aiplay(_, msg):
 
         # Create buttons for different playlists
         buttons = [
-            [
-                InlineKeyboardButton("English", callback_data='English'),
-            ],
-            [
-                InlineKeyboardButton("Hindi", callback_data='Hindi'),
-            ],
-            [
-                InlineKeyboardButton("Other Languages", callback_data='OtherLanguages')
-            ]
+            [InlineKeyboardButton("English", callback_data='English')],
+            [InlineKeyboardButton("Hindi", callback_data='Hindi')],
+            [InlineKeyboardButton("Other Languages", callback_data='OtherLanguages')]
         ]
 
         # Send the buttons to the user
@@ -38,18 +30,18 @@ def aiplay(_, msg):
         print(f"Error: {e}")
         msg.reply_text("An error occurred while processing your request.")
 
-@app.on_callback_query()
+@app.on_callback_query(filters.regex("^(English|Hindi|OtherLanguages)$"))
 def button_callback(_, callback_query):
     try:
         chat_id = callback_query.message.chat.id
         user_id = callback_query.from_user.id
         playlist_name = callback_query.data
 
-        # Get the playlist ID based on the user's choice
-        playlist_id = SPOTIFY_PLAYLISTS.get(playlist_name)
+        # Get the playlist URL based on the user's choice
+        playlist_url = SPOTIFY_PLAYLISTS.get(playlist_name)
 
         # Get the first track from the selected Spotify playlist
-        playlist_tracks = Spotify.playlist_tracks(playlist_id)
+        playlist_tracks = Spotify.playlist_tracks(playlist_url)
         track_url = playlist_tracks['items'][0]['track']['external_urls']['spotify']
 
         # Start playing the track on the voice chat
@@ -59,4 +51,3 @@ def button_callback(_, callback_query):
     except Exception as e:
         print(f"Error: {e}")
         callback_query.message.reply_text("An error occurred while processing your request.")
-
