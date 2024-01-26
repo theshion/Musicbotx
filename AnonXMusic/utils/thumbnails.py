645fc61cd9a1aaa131cf037ed12dcfa54ed3a3f6
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2024-01-26 13:52:56 +0530

    Update thumbnails.py

--- AnonXMusic/utils/thumbnails.py
+++ AnonXMusic/utils/thumbnails.py
@@ -77,7 +77,7 @@ async def get_thumb(videoid, user_id):
         try:
             async for photo in app.get_chat_photos(user_id,1):
                 sp=await app.download_media(photo.file_id, file_name=f'{user_id}.jpg')
-         except:
+        except:
             async for photo in app.get_chat_photos(app.id,1):
                 sp=await app.download_media(photo.file_id, file_name=f'{app.id}.jpg')
         xp=Image.open(sp)
