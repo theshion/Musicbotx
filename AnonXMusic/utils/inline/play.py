bc66bb7bfdbeae55c5ab873e9765aa62944f9409
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2024-01-28 15:32:08 +0530

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -58,7 +58,7 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="⥑⥏", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="⟳", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="⥤", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▪", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="▫", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
             InlineKeyboardButton(
@@ -66,6 +66,7 @@ def stream_markup_timer(_, chat_id, played, dur):
                 callback_data="GetTimer",
             )
         ],
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
 
@@ -77,8 +78,9 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="⥑⥏", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="⟳", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="⥤", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▪", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="▫", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
 
