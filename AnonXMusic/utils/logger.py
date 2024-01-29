bd2b38aafd08a2e39c3d39448a64992962e54afb
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2024-01-29 22:18:25 +0530

    Update logger.py

--- AnonXMusic/utils/logger.py
+++ AnonXMusic/utils/logger.py
@@ -2,28 +2,32 @@ from pyrogram.enums import ParseMode
 
 from AnonXMusic import app
 from AnonXMusic.utils.database import is_on_off
-from config import LOGGER_ID
+from config import LOGGER_ID, LOGS_ID
 
 
 async def play_logs(message, streamtype):
     if await is_on_off(2):
         logger_text = f"""
-<b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
+<u><b>{app.mention} ꜱᴛʀᴇᴀᴍ ʟᴏɢ</b></u>
 
-<b>ᴄʜᴀᴛ ɪᴅ :</b> <code>{message.chat.id}</code>
-<b>ᴄʜᴀᴛ ɴᴀᴍᴇ :</b> {message.chat.title}
-<b>ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.chat.username}
+⬝ <b>ǫᴜᴇʀʏ :-</b> {message.text.split(None, 1)[1]}
 
-<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>
-<b>ɴᴀᴍᴇ :</b> {message.from_user.mention}
-<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}
+↝<b>ᴇᴠᴇɴ ᴀᴛ⥮</b>
+⬝<b>ᴄʜᴀᴛ ɪᴅ :-</b> <code>{message.chat.id}</code>
+⬝<b>ᴄʜᴀᴛ ɴᴀᴍᴇ :-</b> {message.chat.title}
+⬝<b>ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :-</b> @{message.chat.username}
 
-<b>ǫᴜᴇʀʏ :</b> {message.text.split(None, 1)[1]}
-<b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}"""
-        if message.chat.id != LOGGER_ID:
+↝<b>ǫᴜᴇʀʏ ʙʏ⥮</b>
+⬝ <b>ᴜsᴇʀ ɪᴅ :-</b> <code>{message.from_user.id}</code>
+⬝ <b>ɴᴀᴍᴇ :-</b> {message.from_user.mention}
+⬝ <b>ᴜsᴇʀɴᴀᴍᴇ :-</b> @{message.from_user.username}
+
+↝<b>ᴛʏᴘᴇ⥮</b> 
+⬝ <b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :-</b> {streamtype}"""
+        if message.chat.id != LOGS_ID:
             try:
                 await app.send_message(
-                    chat_id=LOGGER_ID,
+                    chat_id=LOGS_ID,
                     text=logger_text,
                     parse_mode=ParseMode.HTML,
                     disable_web_page_preview=True,
