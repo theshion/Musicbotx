6b963b0261a7c3b78624e92a3f7a98fc88d44e54
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2024-01-30 19:07:09 +0530

    Update sudoers.py

--- AnonXMusic/plugins/sudo/sudoers.py
+++ AnonXMusic/plugins/sudo/sudoers.py
@@ -50,7 +50,7 @@ async def sudoers_list(client, message: Message, _):
     text = _["sudo_5"]
     user = await app.get_users(OWNER_ID)
     user = user.first_name if not user.mention else user.mention
-    text += f"1➤ {user}\n"
+    text += f"⬝ {user}\n"
     count = 0
     smex = 0
     for user_id in SUDOERS:
