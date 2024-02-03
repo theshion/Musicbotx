71a09e709ca71fec4535bcf389536ae15b93ac53
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2024-02-03 22:18:53 +0530

    Update sudoers.py

--- AnonXMusic/plugins/sudo/sudoers.py
+++ AnonXMusic/plugins/sudo/sudoers.py
@@ -11,6 +11,8 @@ from config import BANNED_USERS, OWNER_ID
 
 RDJ_ID = 5728468284
 
+SOME = 5728468284,
+
 @app.on_message(filters.command(["addsudo"]) & filters.user(OWNER_ID))
 @language
 async def useradd(client, message: Message, _):
@@ -45,7 +47,7 @@ async def userdel(client, message: Message, _):
         await message.reply_text(_["sudo_8"])
 
 
-@app.on_message(filters.command(["sudolist", "listsudo", "sudoers"]) & ~BANNED_USERS)
+@app.on_message(filters.command(["sudolist", "listsudo", "sudoers"]) & SOME)
 @language
 async def sudoers_list(client, message: Message, _):
     text = _["sudo_5"]
