698828f1d56c5d7512dc19beeaae4fe3f4a9130f
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2024-02-07 14:34:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ UPSTREAM_REPO = getenv(
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
-    "GIT_TOKEN", "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"
+    "GIT_TOKEN", "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"
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/NoahMusicUpdates")
